d0634ece885bfd06560211b05fd11e53f95ea535
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-09-24 01:32:50 +0200

    Added MeViewController Class

---
+++ BreakPoint/BreakPoint/Controller/MeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MeViewController.swift
+//  BreakPoint
+//
+//  Created by Ahmed Elbasha on 9/24/18.
+//  Copyright © 2018 Ahmed Elbasha. All rights reserved.
+//
+
+import UIKit
+
+class MeViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func signOutButtonWasPressed(_ sender: Any) {
+        // User Sign Out Implementation Goes Here.
+    }
+
+}
